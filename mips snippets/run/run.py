e694c3c4a3a8466a6222a7a8c65601fed300c802
TropicSapling <tropical.productions.scratch@gmail.com>
2021-05-27 19:50:23 +0200

    Tab "indentation"
    "Arguments" to assembly instructions are now indented so that they line up with the "arguments" of other instructions.

--- mips snippets/run/run.py
+++ mips snippets/run/run.py
@@ -3,30 +3,31 @@ import os
 import glob
 import re
 
-
 with open('MIPS.txt') as file:
     linhas = file.readlines()
-
-
-
 
 # for arquivo in glob.glob('../*.sublime-snippet'):
 #     print('Removendo:', arquivo)
 #     os.remove(arquivo)
 
-
-
 for linha in linhas:
     pedacos = re.split(r'\s{2,}', linha)
-
-
+    
+    length = 0
+    for c in pedacos[1]:
+        if c == ' ':
+            break
+        
+        length += 1
+    
+    if length < 4:
+        tabs = "\t\t"
+    else:
+        tabs = "\t"
 
     funcName = pedacos[0]
-    CDATA = pedacos[1]
+    CDATA = pedacos[1][0:length]+tabs+pedacos[1][length+1:]
     comentario = pedacos[2].strip()
-
-
-
 
     content = '''<!-- THIS WAS AUTOMATED GENERATED -->
 <snippet>
@@ -35,8 +36,6 @@ for linha in linhas:
     <tabTrigger>'''+funcName+'''</tabTrigger>
     <scope>source.mips</scope>
 </snippet>\n'''
-
-
 
     fileDest = '../'+funcName+'.sublime-snippet'
     with open(fileDest, 'w') as file:
